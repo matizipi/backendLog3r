e08b92f14ec17ab5338b63266a231eb2614cf750
matizipi <matizipi123@4146c987a9a6>
2024-05-01 22:20:39 +0000

    pycache

--- flask_app.py
+++ flask_app.py
@@ -5,11 +5,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-    return "<p>Hello, World!</p>"
-
-@app.route('/prueba')
-def prueba():
-    return 'Esta vivo!'
+    return 'Hello from Flask!'
 
