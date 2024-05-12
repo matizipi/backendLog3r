cedcb0fc6689335b71bfe1a50d88f64f9592cf32
estelucero <este.lucero2212@gmail.com>
2024-05-12 18:57:07 -0300

    Limpio repo

--- README.md
+++
@@ -1,2 +0,0 @@
-# backendLog3r
-Backend en python del trabajo práctico principal de PP1
--- mongoDB.py
+++ mongoDB.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-import json
+import certifi
 
 # Configuración de la conexión a MongoDB
 MONGO_HOST = 'mongodb+srv://pp1-rf-user:q0cK1qe153bpS5I0@cluster.0zuctio.mongodb.net'
@@ -7,19 +7,24 @@ MONGO_PORT = 27017
 MONGO_DB = 'pp1_rf'
 
 # Crear una instancia de MongoClient
-#client = MongoClient(MONGO_HOST, MONGO_PORT)
+client = MongoClient(MONGO_HOST, MONGO_PORT,tlsCAFile=certifi.where())
 
 # Obtener una referencia a la base de datos
-#db = client[MONGO_DB]
+db = client[MONGO_DB]
 
-def searchMdb(label):
+def searchMdb():
     # Realizar operaciones con la base de datos MongoDB
     # Por ejemplo, puedes obtener una colección y devolver algunos documentos
-    #collection = db['usuarios']
+    collection = db['usuarios']    
     result = []
-    filtro = {"label": label}
-    documento = collection.find_one(filtro, {"_id": 0,"label":0})
-    # Serializar el diccionario como JSON
-    json_data = json.dumps(documento)
-    result = json.loads(json_data)
-    return result
+    filtro = {}
+    cursor = collection.find(filtro)
+    return cursor
+    
+    
+
+
+if __name__== "__main__":
+   
+    searchMdb()
+    
