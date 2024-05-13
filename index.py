962c0b937730b0ef61849bb6525cdf53552b8983
Alfonso lombardi <ajavierlombardi@gmail.com>
2024-05-13 15:26:43 -0300

    fix dateTime + cors

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -14,9 +15,11 @@ from bson import json_util
 from waitress import serve
 ## variable global para ir guardando el ultimo label usado en el modelo
 ultimo_Label = 0
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
 def home():
@@ -57,20 +60,29 @@ def login():
 @app.route('/api/register', methods=['POST'])
 def register():
     return 
-
-@app.route('/api/authentication/logs',methods=['POST'])
+@app.route('/api/authentication/logs', methods=['POST'])
 def logs():
     data = request.form
     mensaje = data.get('mensaje')
-    horario = data.get('horario')
+    horario_str = data.get('horario')  # Assuming the date is passed as a string
     dni = data.get('dni')
+    
     try:
-        resultado = registrarLog(mensaje,horario,dni) 
+        # Convert string to datetime object
+        horario = datetime.strptime(horario_str, '%Y-%m-%d %H:%M:%S')  # Adjust the format if necessary
+        
+        # Now you can use horario as a datetime object in your registrarLog function
+        resultado = registrarLog(mensaje, horario, dni) 
+        
         return jsonify(resultado), 200
     except Exception as e:
-        # Si se produce un error, devuelve un código de estado HTTP 500 y un mensaje de error
+        # If an error occurs, return a 500 HTTP status code and an error message
         mensaje_error = "Error interno en el servidor: {}".format(str(e))
-        return jsonify({'error': mensaje_error}), 500     
+        return jsonify({'error': mensaje_error}), 500
+    
+
+
+
 
 if __name__== "__main__":
     # development
