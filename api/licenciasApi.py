fe6ca3dee62fd9a33f258404c28a747df81f36d4
Alfonso lombardi <ajavierlombardi@gmail.com>
2024-06-04 20:36:04 -0300

    agrego endpoint delete y verificaciones

--- api/licenciasApi.py
+++ api/licenciasApi.py
@@ -1,6 +1,6 @@
 from ast import parse
 from flask import Blueprint, request, jsonify
-from repository.licenciasRepository import newLicense,getLicenses
+from repository.licenciasRepository import deleteLicencia, newLicense,getLicenses
 from datetime import datetime
 
 # Crear instancia Blueprint con el nombre 'licencias'
@@ -20,7 +20,7 @@ def get_licences():
 @licencias_bp.route('/', methods=['POST'])
 def license():
     data = request.form    
-    user_id = data.get('user_id')  # Assuming the date is passed as a string
+    user_id = data.get('user_id')  
     fechaDesde = data.get('fechaDesde')
     fechaHasta = data.get('fechaHasta')     
     try:
@@ -28,9 +28,15 @@ def license():
         fechaDesde = datetime.strptime(fechaDesde, '%Y-%m-%d')
         fechaHasta = datetime.strptime(fechaHasta, '%Y-%m-%d')
         # Verificar que fechaHasta sea posterior a fechaDesde
-        if fechaHasta <= fechaDesde:
-            return jsonify({"error": "Fechas incorrectas"}), 400
-            # Now you can use horario as a datetime object in your registrarLog function
+        # Obtener el día de la semana de fechaDesde
+        dia_semana_fechaDesde = fechaDesde.strftime('%w')
+        dia_semana_fechaHasta = fechaHasta.strftime('%w')
+        diferencia_dias = ((fechaHasta - fechaDesde).days)
+        resto = diferencia_dias % 7
+
+        if dia_semana_fechaDesde != 1 or dia_semana_fechaHasta !=0 or fechaHasta <= fechaDesde or diferencia_dias>35 or resto!=0: 
+            return jsonify({"error": "Fechas incorrectas"}), 400          
+           
 
         # Convertir las fechas al formato deseado
         fechaDesde_str = fechaDesde.strftime('%Y-%m-%d')
@@ -42,3 +48,13 @@ def license():
         # If an error occurs, return a 500 HTTP status code and an error message
         mensaje_error = "Error interno en el servidor: {}".format(str(e))
         return jsonify({'error': mensaje_error}), 500  
+    
+
+@licencias_bp.route('/<user_id>', methods=['DELETE'])
+def delete_licencia(user_id):
+    try:
+        result = deleteLicencia(user_id)
+        return jsonify(result), 200
+    except Exception as e:
+        mensaje_error = "Error interno en el servidor: {}".format(str(e))
+        return jsonify({'error': mensaje_error}), 500    
--- repository/licenciasRepository.py
+++ repository/licenciasRepository.py
@@ -1,5 +1,6 @@
 from bson import ObjectId
 from mongoDB import db
+from datetime import datetime
 
 
 
@@ -22,3 +23,16 @@ def newLicense(userId,fechaDesde,fechaHasta):
         "fechaDesde": fechaDesde,
         "fechaHasta": fechaHasta,
         "userId":  ObjectId(userId)})   
+    
+
+def deleteLicencia(user_id):
+    collection = db['licencias']
+    licencia = collection.find_one({'_id': ObjectId(user_id)})
+    fechaDesde = datetime.strptime(licencia.fechaDesde, '%Y-%m-%d')     
+    fecha_actual = datetime.now()
+    # Formatear la fecha para obtener solo el año, mes y día
+    fecha_de_hoy = fecha_actual.strftime('%Y-%m-%d')
+
+    if licencia and fecha_de_hoy < fechaDesde:
+        result = collection.delete_one({'_id': ObjectId(user_id)})
+    return {'mensaje': 'Licencia eliminada' if result.deleted_count > 0 else 'Licencia no encontrado'}    
